496dc00bab4b94018859e8917c68a109b58c4ee7
shock <shock_cq@163.com>
2018-11-28 16:42:00 +0800

    cli create wallet

--- cli_createwallet.go
+++ cli_createwallet.go
@@ -1,5 +1,11 @@
 package main
 
-func (cli *CLI) createWallet(nodeID string) {
+import "fmt"
 
+func (cli *CLI) createWallet(nodeID string) {
+	wallets, _ := NewWallets(nodeID)
+	address := wallets.CreateWallet()
+	wallets.SaveToFile(nodeID)
+
+	fmt.Printf("Your new address: %s\n", address)
 }
